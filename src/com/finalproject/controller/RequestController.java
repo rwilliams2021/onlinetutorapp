16b4fd9579795c8adb7e8fa9f906c59a77b2143e
rwilliams <richyrichw93@gmail.com>
2021-02-06 17:41:29 +0200

    commit 501

---
+++ src/com/finalproject/controller/RequestController.java
@@ -0,0 +1,8 @@
+package com.finalproject.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RequestController {
+
+}
---
+++ src/com/finalproject/dao/RequestDAOImpl.java
@@ -0,0 +1,41 @@
+package com.finalproject.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.finalproject.domain.Booking;
+import com.finalproject.domain.Request;
+
+@Repository
+public class RequestDAOImpl implements RequestDAO{
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Override
+	public int add(Request request) {
+		String sql = "insert into requests(parentid,tutorid) values (?,?)";
+		Object params[] = {request.getParentid(),request.getTutorid()};
+		int n = jdbcTemplate.update(sql,params);
+		return n;
+	}
+
+	@Override
+	public List<Request> findAll() {
+		String sql = "select * from requests";
+		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Request>(Request.class));
+	}
+
+	@Override
+	public int update(Request request) {
+		String sql = "update bookings set subject=?, duration_in_hours=?, times_per_week=?, email=?, cell_number=? where id=?";
+		Object params[] = {};
+		int n = jdbcTemplate.update(sql,params);
+		return n;
+	}
+
+}
