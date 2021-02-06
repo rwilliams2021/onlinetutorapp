1bcd2ddb285c62ecc748b42974f55b5b379ce9b3
rwilliams <richyrichw93@gmail.com>
2021-02-06 18:01:42 +0200

    requestservice added

--- src/com/finalproject/service/RequestService.java
+++ src/com/finalproject/service/RequestService.java
@@ -1,5 +1,11 @@
 package com.finalproject.service;
 
-public interface RequestService {
+import java.util.List;
 
+import com.finalproject.domain.Request;
+
+public interface RequestService {
+	public int add(Request request);
+	public List<Request> getAll();
+	public int update(Request request);
 }
