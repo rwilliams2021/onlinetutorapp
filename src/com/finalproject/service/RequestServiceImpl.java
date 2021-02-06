01f11a5ddb42f6f17005d88b078f751cae23ef8e
rwilliams <richyrichw93@gmail.com>
2021-02-06 18:34:20 +0200

    requestservice completed

--- src/com/finalproject/service/RequestServiceImpl.java
+++ src/com/finalproject/service/RequestServiceImpl.java
@@ -1,5 +1,34 @@
 package com.finalproject.service;
 
-public class RequestServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.finalproject.dao.RequestDAO;
+import com.finalproject.domain.Request;
+
+@Service
+public class RequestServiceImpl implements RequestService{
+
+	@Autowired
+	private RequestDAO requestDAO;
+	
+	@Override
+	public int add(Request request) {
+		int n = requestDAO.add(request);
+		return n;
+	}
+
+	@Override
+	public List<Request> getAll() {
+		return requestDAO.findAll();
+	}
+
+	@Override
+	public int update(Request request) {
+		int n = requestDAO.update(request);
+		return n;
+	}
 
 }
